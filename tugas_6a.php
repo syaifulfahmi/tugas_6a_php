53de809629c3da94ada1c75b1422b15b543afea3
syaifulfahmi <52196122+syaifulfahmi@users.noreply.github.com>
2020-10-08 12:05:15 +0700

    Add files via upload

---
+++ tugas_6a.php
@@ -0,0 +1,11 @@
+<?php
+
+	$usia_habib = 21;
+	$usia_aji = $usia_habib + 4;
+	$usia_bintan = $usia_aji + 3;
+
+	echo "======================================= <br>";
+	echo "Usia Habib = ".$usia_habib,"<br>","Usia Aji = ".$usia_aji,"<br>","Usia Bintan = ".$usia_bintan;
+	echo "<br>=======================================";
+
+?>
